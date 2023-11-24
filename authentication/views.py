b4b5707851c3a3a5322f3de5807dba396bc0658e
21methods <monojoshua2@gmail.com>
2023-11-24 12:25:19 +0300

    none

--- authentication/views.py
+++ authentication/views.py
@@ -2,30 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 from django import forms
 from django.contrib import messages
 
 
 # Create your views here.
+
 class RegistrationForm(UserCreationForm):
-    '''def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        self.fields["username"].widget.attrs.update({
-            'required':'',
-            'name': 'username',
-            'id':'id_username',
-            'type': 'text', 
-            'placeholder':'username',
-            'maxlength':'15',
-        })'''
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '@username'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '@username', 'min_length': '5'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter your email'}))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name',  'password1', 'password2']
         help_text = {
             'username': '',
             'password1': '',
@@ -50,7 +45,7 @@ def register(request):
 
 
             # Redirect to a success page or login the user
-            return redirect('home')
+            return redirect('authentication/login')
     else:
         form = RegistrationForm()
 
@@ -72,7 +67,23 @@ def register(request):
     '''
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password']
+
+        User = authenticate(username=username, password=password)
+
+        if User is not None:
+            login(request, user)
+            return render(request, 'authentication/home.html', {'first_name' : first_name})
+
+        else:
+            messages.error(request, 'Bad Credentials!')
+            return redirect('login')
+
+
     return render(request, 'authentication/login.html')
 
+@login_required(login_url="/login")
 def home(request):
     return render(request, 'authentication/home.html')
